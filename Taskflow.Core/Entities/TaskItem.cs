54b87734dba97b39a2a7a704f2b9e565857af11b
Maryam Ali <merium455@gmail.com>
2025-10-21 18:38:46 +0500

    Added Core Entities: User, Project, TaskItem, TasksStatus

---
+++ Taskflow.Core/Entities/TaskItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Taskflow.Core.Enums;
+
+namespace TaskFlow.Core.Entities
+{
+    public class TaskItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public TasksStatus Status { get; set; } = TasksStatus.ToDo;
+        public DateTime? DueDate { get; set; }
+
+        // Relationships
+        public int ProjectId { get; set; }
+        public Project? Project { get; set; }
+
+        public int? AssignedUserId { get; set; }
+        public User? AssignedUser { get; set; }
+    }
+}
+
