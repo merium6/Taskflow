06470ba029c34b2f8d9b56907f14cf600573a145
Maryam Ali <meriumali5@gmail.com>
2025-10-21 23:28:13 +0500

    Controllers

---
+++ Taskflow.API/Controllers/UserController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskFlow.Core.Entities;
+using TaskFlow.Core.Services.Interfaces;
+
+namespace Taskflow.API.Controllers
+{
+    [ApiController]
+    [Route("api/users")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _userService.GetAllAsync();
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var user = await _userService.GetByIdAsync(id);
+            if (user == null)
+                return NotFound();
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] User user)
+        {
+            var newUser = await _userService.CreateAsync(user);
+            return CreatedAtAction(nameof(GetById), new { id = newUser.Id }, newUser);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] User user)
+        {
+            if (id != user.Id)
+                return BadRequest();
+
+            await _userService.UpdateAsync(user);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _userService.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
