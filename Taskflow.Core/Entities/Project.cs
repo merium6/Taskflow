3ecedcd76656162dc95295d271be55752bc97b8d
Maryam Ali <meriumali5@gmail.com>
2025-10-21 18:38:46 +0500

    Added Core Entities: User, Project, TaskItem, TasksStatus

---
+++ Taskflow.Core/Entities/Project.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskFlow.Core.Entities
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        // Relationships
+        public int CreatedByUserId { get; set; }
+        public User? CreatedByUser { get; set; }
+        public ICollection<TaskItem>? Tasks { get; set; }
+    }
+}
+
