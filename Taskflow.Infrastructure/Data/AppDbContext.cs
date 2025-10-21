695fcd116a05d4455156ab91b78788ab386061fc
Maryam Ali <merium455@gmail.com>
2025-10-21 19:04:20 +0500

    Added EF Core setup with AppDbContext and MySQL configuration

---
+++ Taskflow.Infrastructure/Data/AppDbContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using TaskFlow.Core.Entities;
+
+namespace TaskFlow.Infrastructure.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Project> Projects { get; set; }
+        public DbSet<TaskItem> TaskItems { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Projects)
+                .WithOne(p => p.CreatedByUser)
+                .HasForeignKey(p => p.CreatedByUserId);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.AssignedTasks)
+                .WithOne(t => t.AssignedUser)
+                .HasForeignKey(t => t.AssignedUserId)
+                .OnDelete(DeleteBehavior.SetNull);
+        }
+    }
+}
+
